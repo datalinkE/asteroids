68bc4c29af95b4493f1654e43b3d48f5d2150638
Andrey Pichugin <a-pichugin@yandex-team.ru>
2014-10-27 19:45:55 +0300

    Initial commit

---
+++ android/src/com/datalink/asteroids/RendererWrapper.java
@@ -0,0 +1,24 @@
+package com.datalink.asteroids;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class RendererWrapper implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		GameLibJNIWrapper.on_draw_frame();		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		GameLibJNIWrapper.on_surface_changed(width, height);		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		GameLibJNIWrapper.on_surface_created();		
+	}
+}
